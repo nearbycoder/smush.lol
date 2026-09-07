912e79d6d19cd7aca6aa6acd327dc4455ed9a613
Josh Hamilton <nearbycoder@gmail.com>
2026-09-07 15:20:04 -0500

    Add print dimensions and DPI calculator

--- web/tools/registry.ts
+++ web/tools/registry.ts
@@ -1,4 +1,5 @@
+import { printTool } from "./print";
 import { histogramTool } from "./histogram";
 import { pickerTool } from "./picker";
 import { paletteTool } from "./palette";
-export const imageTools = { palette: paletteTool, picker: pickerTool, histogram: histogramTool };
+export const imageTools = { palette: paletteTool, picker: pickerTool, histogram: histogramTool, print: printTool };
